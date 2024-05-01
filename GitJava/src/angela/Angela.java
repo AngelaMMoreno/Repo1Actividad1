75416585afe6be2dcdd289dc35a44bd03f34d325
AngelaMMoreno <161309617+AngelaMMoreno@users.noreply.github.com>
2024-05-01 16:54:48 +0200

    Añadido paquete angela con clase Angela y método para imprimir nombre

---
+++ GitJava/src/angela/Angela.java
@@ -0,0 +1,13 @@
+package angela;
+
+public class Angela {
+ 
+    public static void imprimirNombre() {
+        System.out.println("Hola, soy Angela");  
+
+    }
+    public static void main(String[] args) {
+        imprimirNombre();
+    }
+}
+
